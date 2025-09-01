30f71e5e2afab67d29dd49ce1e6d63c6f1d595ad
Efwan016 <efwanrizaldi@gmail.com>
2025-09-01 19:44:42 +0700

    first commit

---
+++ database/migrations/2025_08_31_195654_create_merchant_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('merchant_products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignID('merchant_id')->constrained()->onDelete('cascade');
+            $table->foreignID('product_id')->constrained()->onDelete('cascade');
+            $table->foreignID('warehouse_id')->constrained()->onDelete('cascade');
+            $table->unsignedInteger('stock')->index();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('merchant_products');
+    }
+};
